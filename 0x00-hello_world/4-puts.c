afcab05a854969fd17fdf3c8b70e5f879fc4a7d0
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-16 09:38:04 -0800

    atomic

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts "Programming is like building a multilingual puzzle
+#include<stdio.h>
+/**
+ * the programs does the thing
+ * main - makes the code run
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
