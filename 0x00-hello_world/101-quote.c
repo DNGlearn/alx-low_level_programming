9208987a60e6aa0c191360b17f039fe52abcf9c3
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-16 10:29:21 -0800

    aston

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - yo it yo boi main
+ * Return: 0
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (0);
+}
