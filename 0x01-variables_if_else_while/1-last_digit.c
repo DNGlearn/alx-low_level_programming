23a31597f98c0d237907a04cb3943ac986f25eed
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-17 05:24:46 -0800

    armadillo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,16 +16,16 @@ int main(void)
 	if (a > 5)
 	{
 		printf("Last digit of %d", n);
-		printf(" is %d and is greater than 5", a);
+		printf(" is %d and is greater than 5\n", a);
 	}else
 	if (a == 0)
 	{	printf("Last digit of %d", n);
-		printf(" is %d and is 0", a);
+		printf(" is %d and is 0\n", a);
 	}else
 	if (a < 6)
 	{
 		printf("Last digit of %d", n);
-		printf(" is %d and is less than 6 and not 0", a);
+		printf(" is %d and is less than 6 and not 0\n", a);
         }
 	return (0);
 }
