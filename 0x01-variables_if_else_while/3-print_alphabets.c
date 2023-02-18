2f74d7664d6050005b2944e151ffa01318e62c58
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-17 16:49:57 -0800

    atrophy

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - what's good main
+ * Return: 0
+ */
+int main(void)
+{
+	char upper;
+	char lower;
+
+	upper = 'A';
+	lower = 'a';
+	while
+		(lower <= 'z')
+		{
+			putchar(lower);
+			lower++;
+		}
+	putchar('\n');
+	while
+		(upper <= 'Z')
+		{
+			putchar(upper);
+			upper++;
+		}
+	return (0);
+}
