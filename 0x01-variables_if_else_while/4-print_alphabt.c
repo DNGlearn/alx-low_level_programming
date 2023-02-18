313b07d23714178d70ae94aba38d77323cef1341
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-17 17:10:01 -0800

    argon

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - yo main
+ * Return: 0
+ */
+int main (void)
+{
+	char lower;
+
+	while
+		(lower <= 'z')
+		{
+			if(lower != q || lower !=e)
+			{
+				putchar(lower);
+			}
+			lower++;
+		}
+	putchar('\n');
+	return (0);
+}
