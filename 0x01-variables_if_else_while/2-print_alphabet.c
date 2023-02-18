30767bcb0f26beeebbdb2689c1a65b74acb1156d
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-17 16:23:26 -0800

    assault

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - what's good main
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while 
+		(letter <= 'z')
+		{
+			putchar(letter);
+			letter++;
+		}
+	putchar("\n");
+	return (0);
+}
