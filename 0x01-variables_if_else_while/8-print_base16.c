b0e4bc0d1e564047f62dda13c67ff494f2b6d829
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-17 18:07:32 -0800

    argentina

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - main killed best girl
+ * Return: 0
+ */
+int main(void)
+{
+	int nums;
+	char lets;
+
+	nums = 0;
+	lets = 'a';
+	while
+		(nums < 10)
+		{
+			putchar(nums + '0');
+			nums++;
+		}
+	while
+		(lets < 'g')
+		{
+			putchar(lets);
+			lets++;
+		}
+	putchar('\n');
+	return (0);
+}
