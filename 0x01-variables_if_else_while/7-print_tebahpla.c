9329874b8e3d65663bd7d05c8ddcb7fe21bcd91b
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-17 17:59:17 -0800

    adrenaline

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - dang Sandra
+ * Return: 0
+ */
+int main(void)
+{
+	char lower;
+
+	lower = 'z';
+	while
+		(lower >= 'a')
+		{
+			putchar(lower);
+			lower--;
+		}
+	putchar('\n');
+	return (0);
+}
