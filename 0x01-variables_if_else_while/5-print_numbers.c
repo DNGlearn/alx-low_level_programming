d132d01cb596ada8bcb52a4b226de34fc4121fcf
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-17 17:30:21 -0800

    argon

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - aloha main
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while
+		(a < 10)
+		{
+			printf(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
