af0ccedce85ead6f25bbabc3b91b1430a84f908f
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-20 06:07:52 -0800

    adonis

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - is my boy packin
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	x = 0;
+	y = 0;
+	while
+		(x < 10)
+		{
+			for (y = 0,y < 10,y++)
+			{
+				putchar("%d%d",x,y);
+			}
+			if (x != 8 || y != 9)
+			{
+				putchar(", ");
+			}
+		}
+	return (0);
+}
