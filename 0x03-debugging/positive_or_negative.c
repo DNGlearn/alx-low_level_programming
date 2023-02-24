599491e72f7a58fbcca9abae89e51b878198fc74
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-24 12:49:55 -0800

    brown

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,14 +3,17 @@ void positive_or_negative(int i)
 {
 	if  (i > 0)
 	{
-		 printf("%d is positive\n", i);
+		 printf("%d is positive", i);
+		 printf("\n");
 	}
 	if  (i < 0)
 	{
 		printf("%d is negative\n", i);
+		printf("\n");
 	}
 	if (i == 0)
 	{
 		printf("%d is zero\n", i);
+		printf("\n");
 	}
 }
