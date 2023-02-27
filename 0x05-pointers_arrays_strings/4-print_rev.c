e2e5c93aca6ef4fd58c0bffa0bb2aa140996dd99
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-27 12:52:07 -0800

    ballsy

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - function reverses a string
+ * @*s: this is the pointer that is pointing to a string
+ * @s: the chars
+ */
+void print_rev(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (*s != '0')
+	{
+		*s++;
+		length++;
+	}
+	while (length <= 0)
+	{
+		_putchar(*s);
+		*s--;
+		length--;
+	}
+	_putchar('\n');
+}
