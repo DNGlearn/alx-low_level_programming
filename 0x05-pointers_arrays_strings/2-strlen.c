9c0685e5eedfd000fb8e0fa68054263b84cc0039
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-27 12:00:09 -0800

    browse

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,18 @@
-#include<string.h>
 /**
  * _strlen - function gets the string lenght
  * @*str: pointer to string of length to get
+ * @count: the count
+ * Return: count
  */
 int _strlen(char *s)
 {
-	return (strlen(*s));
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
 }
