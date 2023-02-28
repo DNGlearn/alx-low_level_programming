425abd132625850d84920d9c7d5dd4f33a083a78
DNGlearn <dennis.gitau.learn@gmail.com>
2023-02-28 11:26:39 -0800

    brakes

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,11 +5,21 @@
 void puts_half(char *str)
 {
 	int count = 0;
+	int j;
+
 	int i = 0;
 	while (*str != '\0')
 	{
 		str++;
 		count++;
 	}
-	
+	i = count / 2;
+	for (j = 0; j <= count; j++)
+	{
+		if (j > i)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
 }
