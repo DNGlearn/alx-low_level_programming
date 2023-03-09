db90be7c523b8d30a32c6da345e93e2e8e430285
DNGlearn <dennis.gitau.learn@gmail.com>
2023-03-09 14:20:42 -0800

    bronze

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,15 @@
 /**
  * void _puts_recursion - the function
  * @s: the deets babyyy
- * @i: array count
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-	char p = *s;
-
-	if (*s[i] != '\0')
+	if (*s)
 	{
-		_putchar(p[i]);
-		return (_puts_recursion(p[i+1]));
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	} else
+	{
+		_putchar('\n');
 	}
 }
