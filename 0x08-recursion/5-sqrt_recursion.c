821da70eca2377411411971a5fc09bf997305f91
DNGlearn <dennis.gitau.learn@gmail.com>
2023-03-09 16:55:38 -0800

    dare

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - the func goes on
+ * @n: the nums
+ * Return: the sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	return (ext_sqrt(n, 0));
+}
+/**
+ * ext_sqrt - the func dont stop
+ * @n: the num again
+ * @s: the sqrt
+ * Return: s
+ */
+int ext_sqrt(int n, int s)
+{
+	if (s * s < n)
+	{
+		return (ext_sqrt(n, s + 1));
+	}
+	if (s * s == n)
+	{
+		return (i);
+	}
+}
