58bcbb57fb30a38ad639e7046ab545dafa6b7eef
DNGlearn <dennis.gitau.learn@gmail.com>
2023-03-15 16:11:07 -0700

    drag

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,8 @@ int main(int argc, char* argv[])
 {
 	int i;
 	for (i = 0; i < argc; i++)
-	{
-		if (i == 0)
-		{
-			printf("arg[%d] = %s\n", i, argv[1]);
-		}
+	{	
+		printf("arg[%d] = %s\n", i, argv[i]);
 	}
 	return (0);
 }
