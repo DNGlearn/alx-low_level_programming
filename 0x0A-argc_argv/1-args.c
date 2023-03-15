4beadd669cd739facfb74b9a98c5ca513fabd6f8
DNGlearn <dennis.gitau.learn@gmail.com>
2023-03-15 16:53:01 -0700

    dom

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
  * main - where code is
+ * @argc: the ting
+ * @argv: the tings
  * Return: 0
  */
 int main(int argc, char* argv[])
 {
 	(void) argv;
+
 	printf("%d\n", argc - 1);
 	return (0);
 }
