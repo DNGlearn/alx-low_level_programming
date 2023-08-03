1b05b2ec4ca752bd3df609f9f3e0cb9e75d76b71
DNGlearn <dennis.gitau.learn@gmail.com>
2023-08-03 15:58:12 -0700

    black

--- 0x1C-makefiles/main.c
+++ 0x1C-makefiles/main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	fish();
+	print_holberton();
 	return 0;
 }
 /* School */
--- 0x1C-makefiles/school.c
+++ 0x1C-makefiles/school.c
@@ -2,7 +2,7 @@
 #include<m.h>
 """ now includes the m.h file"""
 
-void fish(void)
+void print_holberton(void)
 {
 	printf("j#0000000000000000000000000000000000000\n");
 	printf("j#000000000000000000@Q**g00000000000000\n");
